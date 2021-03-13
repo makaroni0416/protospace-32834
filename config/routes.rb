7ed18e10a4665e95c053e4bc01b4fa4d69a19758
makaroni0416 <76754228+makaroni0416@users.noreply.github.com>
2021-03-13 20:22:09 +0900

    edit、updateアクション設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "prototypes#index"
-  resources :prototypes, only: [ :new, :create, :show] do
+  resources :prototypes, only: [ :new, :create, :show, :edit, :update] do
   end
   resources :users, only: :show
 end
