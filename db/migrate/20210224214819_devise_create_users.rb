8b0ff1622cb7b287d3d2f8ce349007d3479b53e1
makaroni0416 <76754228+makaroni0416@users.noreply.github.com>
2021-03-15 22:48:04 +0900

    commit

--- db/migrate/20210224214819_devise_create_users.rb
+++ db/migrate/20210224214819_devise_create_users.rb
@@ -6,10 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :name
-      t.text   :profile
-      t.text   :occupation
-      t.text   :position
+
+      t.string :name, null: false
+      t.text :profile, null: false
+      t.text :occupation, null: false
+      t.text :position, null: false
+
 
       ## Recoverable
       t.string   :reset_password_token
@@ -17,6 +19,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
 
       ## Rememberable
       t.datetime :remember_created_at
+
+
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
